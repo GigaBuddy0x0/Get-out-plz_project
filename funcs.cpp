8413cf6a860c8cec2176728489159671aa3f5451
Alex <Alexsmirniy11@gmail.com>
2023-11-13 06:22:14 +0300

    add function MyDiv()

--- funcs.cpp
+++ funcs.cpp
@@ -4,13 +4,16 @@ double MySum(double a, double b)
 {
 return a + b;
 }
-
 double MySub(double a, double b)
 {
 return a - b;
 }
-
 double MyMul(double a, double b)
 {
 return a * b;
 }
+double MyDiv(double a, double b)
+{
+return a / b;
+}
+
